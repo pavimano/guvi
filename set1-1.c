c609945c72d2d6614643a88822f16d63345949dc
pavimano <36395382+pavimano@users.noreply.github.com>
2018-03-20 17:37:51 +0530

    Create set1-1.c

---
+++ set1-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a;
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("positive");
+	}
+	if(a<0)
+	{
+		printf
+		("negative");
+	}
+	if(a=0)
+	{
+		printf("zero");
+	}
+		return 0;
+}
